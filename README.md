e7aad122a4dbb9c234fa416e7375d8fed05df685
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-08-08 11:46:15 -0400

    first

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 leaflet.shapefile
 =================
+
+requires [shapefile-js](https://github.com/calvinmetcalf/shapefile-js) plus if you want it
+to be done in a worker, you'll need [catiline](https://github.com/calvinmetcalf/catiline).
+
+usage:
+
+```javascript
+new L.Shapefile(arrayBuffer or url[,options]);
+
+L.shapefile(arrayBuffer or url[,options]);
+```
+
+options are passed to L.Geojson as is first argument is either an array buffer of a zipped shapefile,
+the url to a zipped shapefile, or the url of the file.shp (this assumes file.dbf exists).
---
+++ leaflet.shpfile.js
@@ -0,0 +1,32 @@
+L.Shapefile =L.GeoJSON.extend({
+    initialize: function (file, options) {
+        if(typeof cw !== 'undefined'){
+            this.worker = cw(function(data,cb){
+                importScripts('shp.js');
+	            shp(data).then(cb);
+            });
+        }
+        L.GeoJSON.prototype.initialize.call(this,{features:[]},options);
+        this.addFileData(file);
+    },
+    addFileData:function(file){
+        var self = this;
+        if(typeof file !== 'string' && !('byteLength' in file)){
+            return self.addData(file);
+        }
+        if(self.worker){
+            self.worker.data(file).then(function(data){
+                self.addData(data);
+            });
+        }else{
+            shp(file).then(function(data){
+                self.addData(data);
+            });
+        }
+        return this;
+    }
+});
+
+L.shapefile= function(a,b){
+    return new L.Shapfile(a,b);
+}
